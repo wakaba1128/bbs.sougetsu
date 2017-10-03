9d7554d8ef3600178b06f565eb4413fe7df96207
wakaba <wakaba1128@users.noreply.github.com>
2017-10-03 11:27:37 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-Hant-TW">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>霜月字幕組論壇</title>
+</head>
+<body>
+<div>
+    <p>本站已結束營運，相關資訊請洽<a href="http://sougetsu.org">網站首頁</a></p>
+</div>
+</body>
+</html>
